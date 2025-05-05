a2d077999a73a77d010bfbfcdc7e2ff265e6a82b
HOÀNG TẤN PHÚ QUỐC <hoangtanphuquoc@gmail.com>
2025-05-05 07:12:08 +0700

    feat (profile-service): update profile service endpoints

--- profile-service/src/main/java/com/phuquocchamp/profileservice/application/dto/ServiceInfoDto.java
+++ profile-service/src/main/java/com/phuquocchamp/profileservice/application/dto/ServiceInfoDto.java
@@ -1,12 +1,20 @@
 package com.phuquocchamp.profileservice.application.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
+@Component
+@ConfigurationProperties(prefix = "info")
+@RefreshScope
 @Getter @Setter
+@JsonIgnoreProperties({"targetSource", "beanFactory", "beanExpressionResolver"})
 public class ServiceInfoDto {
     private Map<String, String> build;
-    private Map<String, String> application;
+    private Map<String, String> contact;
 }
